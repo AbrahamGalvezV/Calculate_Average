84e816e2f83c15c8f2e6829b229fbb8fd25e3157
AbrahamGalvezV <2abrahamtattoosk8@gmail.com>
2024-11-08 11:34:18 +0100

    update in html,css and js

--- main/calculadora.js
+++ main/calculadora.js
@@ -1,27 +1,22 @@
 
 function calculateAverage() {
-  // Obtener el valor del input
-  let input = document.getElementById("qualifications").value;
 
-  if (!input) {
-    document.getElementById("result").textContent = "No se ingresaron notas";
-    return;
-  }
-  // Convierte en un array de números, filtrando los no válidos
-  const qualifications = input.split(",").map(Number).filter(qualifications => !isNaN(qualifications));
+  let inputElement = document.getElementById('qualifications');
+  inputElement.value = inputElement.value.replace(/[^0-9.,]/g,'');
+  let input = inputElement.value;
 
-  if (qualifications.length === 0) {
-    document.getElementById("result").textContent = "No se registraron notas válidas";
-    return;
-  }
+    // Convierte en un array de números, filtrando los no válidos
+    const qualifications = input.split(",").map(Number).filter(note => !isNaNñ(note));
+
+    // Calcula la suma del promedio
+    const sum = qualifications.reduce((accumulator, note)=> accumulator + note, 0);
+    const average = sum / qualifications.length;
   
-  // Calcula la suma del promedio
-  const suma = qualifications.reduce((acumulador, qualifications)=> acumulador + qualifications, 0);
-  const promedio = suma / qualifications.length;
+    // Muestra el resultado en el pàrrafo con id "result"
+    document.getElementById("result").textContent = average.toFixed(2); 
+  
+  }
 
-  // Muestra el resultado en el pàrrafo con id "result"
-  document.getElementById("result").textContent = `El promedio es: ${promedio.toFixed(2)}`; 
 
-} 
 // Añade un evento que ejecuta la función cada vez que el usuario escribe en el input
 document.getElementById('qualifications').addEventListener('input', calculateAverage);
