1bbaa2943cc1a10791671edfc5b00e56ac5e0287
AbrahamGalvezV <2abrahamtattoosk8@gmail.com>
2024-11-11 11:08:38 +0100

    Js update

--- main/calculadora.js
+++ main/calculadora.js
@@ -2,11 +2,18 @@
 function calculateAverage() {
 
   let inputElement = document.getElementById('qualifications');
+
   inputElement.value = inputElement.value.replace(/[^0-9.,]/g,'');
+
   let input = inputElement.value;
 
     // Convierte en un array de números, filtrando los no válidos
-    const qualifications = input.split(",").map(Number).filter(note => !isNaNñ(note));
+    const qualifications = input.split(",").map(Number).filter(note => !isNaN(note));
+
+    if (qualifications.length === 0) {
+      document.getElementById("result").textContent = "Por favor, ingresa al menos un número válido.";
+      return;
+  }
 
     // Calcula la suma del promedio
     const sum = qualifications.reduce((accumulator, note)=> accumulator + note, 0);
@@ -16,7 +23,6 @@ function calculateAverage() {
     document.getElementById("result").textContent = average.toFixed(2); 
   
   }
-
 
 // Añade un evento que ejecuta la función cada vez que el usuario escribe en el input
 document.getElementById('qualifications').addEventListener('input', calculateAverage);
